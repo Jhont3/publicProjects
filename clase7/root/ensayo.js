1f2b85c5608bcfd91653f9050186fb68e748891a
Jhontwebdesign <100018519+Jhontwebdesign@users.noreply.github.com>
2022-06-02 11:07:33 -0500

    Add files via upload

---
+++ clase7/root/ensayo.js
@@ -0,0 +1,24 @@
+
+// function x(numi) {
+//     while((numi%2)>0){
+//         Math.floor((numi/2)+1);
+//     }
+//     return numi
+// }
+// x(3);
+
+// let numI = 11;
+// numI%2!=0 ? numI/2+1 : console.log("No es impar");
+
+// console.log(numI);
+
+
+// // impares 1-100
+// numeros = []
+// numero = 0
+// while numero<100:
+//     numero+=1
+//     if numero%2 != 0:
+//         numeros.append(numero)#Añade el numero al array
+// print(sum(numeros)) #Imprime la suma de todos los elementos del array
+
